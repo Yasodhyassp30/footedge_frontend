aed37e1e6ee7815e1f2e24bfb89cbe977c6a3121
Yasodhya Perera <82106487+Yasodhyassp30@users.noreply.github.com>
2023-12-21 21:35:00 +0530

    pre drawn graphs are replaced by d3 plots

--- src/pages/dashboard/components/soccerfield.tsx
+++ src/pages/dashboard/components/soccerfield.tsx
@@ -1,37 +1,42 @@
-// src/components/SoccerFieldGraph.tsx
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
+import { players } from "./teamActivity";
+import d3Dsv from "d3-dsv";
+interface SoccerfieldProps {
+  data: players[];
+}
 
-const Soccerfield: React.FC = () => {
+const Soccerfield: React.FC<SoccerfieldProps> = ({ data }) => {
   const svgRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     if (svgRef.current) {
       const svg = d3.select(svgRef.current);
-      const svgHeight = svgRef.current.clientHeight;
-      const svgWidth = svgRef.current.clientWidth;
-
       svg
-      .append("rect")
-      .attr("width", "100%")
-      .attr("height", "100%")
-      .attr("fill", "None")
-      .attr("stroke", "black")
-      .attr("stroke-width", 5);
+        .append("rect")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("fill", "green")
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
+
       svg
         .append("circle")
         .attr("cx", "50%")
         .attr("cy", "50%")
         .attr("r", "10%")
         .attr("fill", "none")
-        .attr("stroke", "black").attr("stroke-width", 5);
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
+
       svg
         .append("circle")
         .attr("cx", "12%")
         .attr("cy", "50%")
         .attr("r", "10%")
         .attr("fill", "none")
-        .attr("stroke", "black").attr("stroke-width", 5);
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
 
       svg
         .append("circle")
@@ -39,23 +44,29 @@ const Soccerfield: React.FC = () => {
         .attr("cy", "50%")
         .attr("r", "10%")
         .attr("fill", "none")
-        .attr("stroke", "black").attr("stroke-width", 5);
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
+
       svg
         .append("rect")
         .attr("x", "0%")
         .attr("y", "22.5%")
         .attr("width", "16%")
         .attr("height", "55%")
-        .attr("fill", "white")
-        .attr("stroke", "black").attr("stroke-width", 5);
+        .attr("fill", "green")
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
+
       svg
         .append("rect")
         .attr("x", "84%")
         .attr("y", "22.5%")
         .attr("width", "16%")
         .attr("height", "55%")
-        .attr("fill", "white")
-        .attr("stroke", "black").attr("stroke-width", 5);
+        .attr("fill", "green")
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
+
       svg
         .append("rect")
         .attr("x", "0%")
@@ -63,7 +74,8 @@ const Soccerfield: React.FC = () => {
         .attr("width", "6%")
         .attr("height", "25%")
         .attr("fill", "none")
-        .attr("stroke", "black").attr("stroke-width", 5);
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
 
       svg
         .append("rect")
@@ -71,9 +83,9 @@ const Soccerfield: React.FC = () => {
         .attr("y", "37.5%")
         .attr("width", "6%")
         .attr("height", "25%")
-        .attr("fill", "white")
-        .attr("stroke", "black").attr("stroke-width", 5);
-
+        .attr("fill", "green")
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
 
       svg
         .append("line")
@@ -81,35 +93,45 @@ const Soccerfield: React.FC = () => {
         .attr("y1", "0%")
         .attr("x2", "50%")
         .attr("y2", "100%")
-        .attr("stroke", "black").attr("stroke-width", 5);
-  
-        svg
-        .selectAll("circle.points")
-        .data([])
-        .enter()
-        .append("circle")
-        .attr("class", "points")
-        .attr("cx", (d) => (d[0] / 100) * svgWidth) 
-        .attr("cy", (d) => (d[1] / 100) * svgHeight) 
-        .attr("r", 5)
-        .attr("fill", "red")
-        .attr("stroke", "black")
-        .attr("stroke-width", 3);
-  
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
     }
   }, []);
+  useEffect(() => {
+    if (svgRef.current) {
+      const svg = d3.select(svgRef.current);
+
+      svg.selectAll("circle.point").remove();
+      svg
+        .selectAll("circle.point")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "point") 
+        .attr("cx", (d) => `${(d.coordinates[0]/1680)*100}%`)
+        .attr("cy", (d) => `${(d.coordinates[1]/1080)*100}%`)
+        .attr("r", 5)
+        .attr("fill", (d) => `rgb(${d.color[2]}, ${d.color[1]}, ${d.color[0]})`)
+        .attr("stroke", "white")
+        .attr("stroke-width", 5);
+    }
+
+    
+  }, [data]);
+  
 
   return (
-<div style={{height: "100%", minHeight: "320px", minWidth: "480px", }}>
-  <svg
-    ref={svgRef}
-    width="480"
-    height="320"
-    style={{ display: "block", width: "100%", height: "100%", }}
-  >
-    {/* SVG content will be rendered here */}
-  </svg>
-</div>
+    <div style={{ height: "100%", minHeight: "320px", minWidth: "480px" }}>
+      <svg
+        ref={svgRef}
+        width="480"
+        height="320"
+        viewBox="0 0 480 320"
+        preserveAspectRatio="xMidYMid meet"
+        style={{ display: "block",height: "100%",width:"100%" }}></svg>
+
+
+    </div>
   );
 };
 
--- src/pages/dashboard/components/teamActivity.tsx
+++ src/pages/dashboard/components/teamActivity.tsx
@@ -6,42 +6,40 @@ import Soccerfield from './soccerfield';
 interface TeamActivityProps {
     socket: Socket | null;
   }
+export interface players{
+  team:number,
+  color:number[],
+  coordinates:number[]
+}
 interface ImageData {
     frame: string[];
-    topview: string[];
-    kde1: string[];
-    kde2: string[];
+    info: players[][];
   }
 const TeamActivity: React.FC<TeamActivityProps> = ({ socket }) => {
     const [imageData, setImageData] = useState<ImageData>({
         frame: [],
-        topview: [],
-        kde1: [],
-        kde2: [],
+        info: []
       });
       const [slider,setSlider] =useState<number>(0)
     useEffect(() => {
         if (socket) {
           socket.on("message_from_server", (data) => {
-    
-            const  imageBase64_frame = data.frame;
-            setImageData(prevState => ({ ...prevState, frame: [...prevState.frame, `data:image/jpeg;base64, ${imageBase64_frame}`] }));
-            
-            const imageBase64_topview = data.topview;
-            setImageData(prevState => ({ ...prevState, topview: [...prevState.topview, `data:image/jpeg;base64, ${imageBase64_topview}`] }));
-            
-            const imageBase64_team1 = data.kde_team1;
-            setImageData(prevState => ({ ...prevState, kde1: [...prevState.kde1, `data:image/jpeg;base64, ${imageBase64_team1}`] }));
-            
-            const imageBase64_team2 = data.kde_team2;
-            
-            setImageData(prevState => ({ ...prevState, kde2: [...prevState.kde2, `data:image/jpeg;base64, ${imageBase64_team2}`] }));
+            console.log(data)
+            setImageData((prevState) => ({
+              frame: [...prevState.frame, `data:image/jpeg;base64, ${data.frame}`],
+              info: [...prevState.info, data.info],
+            }));
+            console.log(imageData.info)
             if (imageData.frame.length==1){
               setSlider(1)
             }
           });
         }
       }, [socket]);
+
+      useEffect(() => {
+        console.log(imageData.info);
+      }, [imageData.info])
   return (
     <div>
               <Grid container spacing={0}>
@@ -59,48 +57,40 @@ const TeamActivity: React.FC<TeamActivityProps> = ({ socket }) => {
           width:"100%",
           padding:"10px"
         }}>
-        {imageData.topview.length!==0  && (
-        <img src={imageData.topview[slider-1]} className="result_image" alt="Received Image2" />
-      )}
+               {imageData.info[slider-1]!==undefined ? (
+          <Soccerfield data={imageData.info[slider-1]} />
+        ) : (
+          <div></div>
+        )}
         </Grid>
         <Grid item xs={12} md={6} sx={{
           minHeight:"320px",
           width:"100%",
           padding:"10px"
         }}>
-        {imageData.kde1.length!==0 && (
-         <img src={imageData.kde1[slider-1]} className="result_image" alt="Received Image3" />
-      )}
+                {imageData.info[slider-1]!==undefined ? (
+          <Soccerfield data={imageData.info[slider-1]} />
+        ) : (
+          <div></div>
+        )}
         </Grid>
         <Grid item xs={12} md={6} sx={{
           minHeight:"320px",
           width:"100%",
           padding:"10px"
         }}>
-        {imageData.kde2.length!==0  && (
-        <img src={imageData.kde2[slider-1]} className="result_image" alt="Received Image4" />
-      )}
-        </Grid>
-        <Grid item xs={12} md={6} sx={{
-          minHeight:"320px",
-          width:"100%",
-          padding:"10px"
-        }}>
-          <Soccerfield/>
-        </Grid>
-        <Grid item xs={12} md={6} sx={{
-          minHeight:"320px",
-          width:"100%",
-          padding:"10px"
-        }}>
-       <Soccerfield/>
+       {imageData.info[slider-1]!==undefined ? (
+          <Soccerfield data={imageData.info[slider-1]} />
+        ) : (
+          <div></div>
+        )}
         </Grid>
         <Grid item xs={1} md={3}></Grid>
         <Grid item xs={10} md={6} sx={{
           width:"100%",
           padding:"10px"
         }}>
-          {imageData.frame.length!==0 && <Slider
+          {imageData.info.length !==0 && <Slider
         aria-label="Temperature"
         defaultValue={slider}
         valueLabelDisplay="auto"
