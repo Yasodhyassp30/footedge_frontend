a9e0c2083dee4b8c96f10d9bab3bf9c6255d220e
Malaka Rodrigo <shehanmalakarodrigo@gmail.com>
2024-02-17 22:05:08 +0530

    feat: add scouting dashboard
    MALAKA

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './pages/dashboard';
-import Soccerfield from './pages/dashboard/components/soccerfield';
-import DensityPlot from './pages/dashboard/components/kdePlot';
-import Navbar from './pages/navbar';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./pages/dashboard";
+import Soccerfield from "./pages/dashboard/components/soccerfield";
+import DensityPlot from "./pages/dashboard/components/kdePlot";
+import Navbar from "./pages/navbar";
+import ScoutingDashboard from "./pages/scouting/ScoutingDashBoard";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Dashboard/>
+      <Navbar />
+      {/* {      <Dashboard/>} */}
+      {/* {      <ScoutingDashboard />} */}
     </div>
   );
 }
