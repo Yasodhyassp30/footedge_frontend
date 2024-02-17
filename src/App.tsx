8e431080b77da7582d561f15a8d104176b9c4fac
Akashmihi12 <seramakash@gmail.com>
2024-02-17 16:34:12 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,20 @@ import Dashboard from './pages/dashboard';
 import Navbar from './pages/navbar/navbar';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// Import the Sidebar component
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter> 
-      <div className="app-container" style={{ display: 'flex' }}>  
-        <div className="content-area"> 
-          <Navbar /> 
-          <Routes>
-            <Route path="/" element={<Login />} /> 
-            <Route path="/register" element={<Register />} /> 
-            <Route path="/dashboard" element={<Dashboard />} /> 
-          </Routes>
-        </div>
+    <div className="app-container" style={{ display: 'flex' }}>
+      <div className="content-area">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
