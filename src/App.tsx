56ad0cdb385b2e0e12c37a71a6931099f0f2b451
Yasodhya Perera <82106487+Yasodhyassp30@users.noreply.github.com>
2024-02-17 19:22:14 +0530

    Revert "Merge pull request #1 from Yasodhyassp30/new-ui"
    This reverts commit c8011b3f6410a8240bbba6237139ab03a30ce5ca, reversing
    changes made to 40147835251bc6fed460f06239fd120d28491b7b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Dashboard from './pages/dashboard';
-import Navbar from './pages/navbar/navbar';
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// Import the Sidebar component
+import Soccerfield from './pages/dashboard/components/soccerfield';
+import DensityPlot from './pages/dashboard/components/kdePlot';
+import Navbar from './pages/navbar';
 
 function App() {
   return (
-    <BrowserRouter> 
-      <div className="app-container" style={{ display: 'flex' }}>  
-        <div className="content-area"> 
-          <Navbar /> 
-          <Routes>
-            <Route path="/" element={<Login />} /> 
-            <Route path="/register" element={<Register />} /> 
-            <Route path="/dashboard" element={<Dashboard />} /> 
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Navbar/>
+      <Dashboard/>
+    </div>
   );
 }
 
