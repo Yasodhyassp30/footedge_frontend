3bc7a9b52af82fbd828bb2390734f72166dde931
Malaka Rodrigo <shehanmalakarodrigo@gmail.com>
2024-07-09 22:37:04 +0530

    refactor: remove scout request

--- src/pages/scouting/scoutResultPage/ScoutResult.tsx
+++ src/pages/scouting/scoutResultPage/ScoutResult.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Spin } from "antd";
+import { Button, Image, Spin } from "antd";
 import React, { useEffect, useState } from "react";
 import { SCOUTING_SERVICE_URL } from "../../../constants/scoutingConstants";
 import { ScoutRequest } from "../../../types/scoutingTypes";
@@ -12,6 +12,8 @@ const ScoutResult: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [activeRequests, setActiveRequests] = useState<Array<ScoutRequest>>([]);
   const [highlight, setHighlight] = useState<number>(0);
+  const [sliderValue, setSliderValue] = useState<number>(1);
+  const [image, setImage] = useState<string | null>(null);
 
   useEffect(() => {
     setLoading(true);
@@ -45,20 +47,57 @@ const ScoutResult: React.FC = () => {
       setHighlight(7);
     } else if (value === 14 || value === 16) {
       setHighlight(8);
-    }else{
+    } else {
       setHighlight(-1);
     }
   };
 
+  const handleSliderChange = async (value: number, req: any) => {
+    try {
+      const url = `${SCOUTING_SERVICE_URL}/scout/frames?image_id=${value}&skill=${req.skill}&unique_id=${req.unique_id}`;
+      const response = await fetchData(url);
+      if (!response) {
+        throw new Error("Network response was not ok");
+      }
+      const imageUrl = `data:image/png;base64,${response}`;
+      setImage(imageUrl);
+      setSliderValue(value);
+    } catch (error) {
+      console.error("Error fetching image:", error);
+      setImage(null);
+    }
+  };
+
+  const incrementSlider = (req: any) => {
+    const newValue = sliderValue + 1;
+    if (newValue <= 91) {
+      handleSliderChange(newValue, req);
+    }
+  };
+
+  const decrementSlider = (req: any) => {
+    const newValue = sliderValue - 1;
+    if (newValue >= 1) {
+      handleSliderChange(newValue, req);
+    }
+  };
+
+  console.log(image);
   return (
     <Spin spinning={loading}>
       {activeRequests.map((request) => (
-        <div className="scout-result-container">
+        <div className="scout-result-container" key={request._id}>
           <div>
             STATUS: {request.status === 2 ? "IN PROGRESS" : "COMPLETED"}
           </div>
-          <ThreeDHumanPose selectedSkill={request} onChange={onChange} />
+          <div className="slider-container">
+            <Button onClick={() => decrementSlider(request)}>-</Button>
+            {image && <Image width={200} src={image} />}
+            <Button onClick={() => incrementSlider(request)}>+</Button>
+            <p>Selected Value: {sliderValue}</p>
+          </div>
           <ResultCard request={request} highlight={highlight} />
+          <ThreeDHumanPose selectedSkill={request} onChange={onChange} />
         </div>
       ))}
     </Spin>
